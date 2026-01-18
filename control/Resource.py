3cdda3e210e79515fa2ce69b2b7746b313f34257
Michal Mastej <m.mastej1812@gmail.com>
2026-01-18 22:45:58 +0100

    Added Resource and RobotState classes

---
+++ control/Resource.py
@@ -0,0 +1,18 @@
+class Resource:
+
+    def __init__(self, resource_id: int, path_id_1: int, path_id_2: int):
+        self.id = resource_id
+        self.paths = (path_id_1, path_id_2)
+        self.priority_list = []
+
+    def get_access(self, robot_id):
+        if robot_id not in self.priority_list:
+            self.priority_list.append(robot_id)
+
+    def release(self, robot_id):
+        if robot_id in self.priority_list:
+            self.priority_list.remove(robot_id)
+
+    def is_first(self, robot_id):
+        return len(self.priority_list) > 0 and self.priority_list[0] == robot_id
+        
